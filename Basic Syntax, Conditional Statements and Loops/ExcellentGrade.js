83546e4b49d539bbd661879c3c99f4181a048624
MariyanMaksimov <58628678+MariyanK1@users.noreply.github.com>
2020-06-21 15:11:45 +0200

    Create ExcellentGrade.js

---
+++ Basic Syntax, Conditional Statements and Loops/ExcellentGrade.js
@@ -0,0 +1,12 @@
+/*
+Write a function that receives a single number and checks if the grade is excellent or not. 
+If it is, print "Excellent", otherwise print "Not excellent"
+*/
+
+function solve(num) {
+    if (num >= 5.50) {
+        return 'Excellent'
+    } else {
+        return 'Not excellent'
+    }
+}
