3bdb6eb30655bc2b3e335d5f13b62ebce0ec6008
MariyanMaksimov <58628678+MariyanK1@users.noreply.github.com>
2020-06-21 15:14:22 +0200

    Create NumbersFromNto1.js

---
+++ Basic Syntax, Conditional Statements and Loops/NumbersFromNto1.js
@@ -0,0 +1,11 @@
+/*
+Create a function that receives a number N and prints all the numbers from N to 1. Try using while loop
+*/
+
+function solve(num) {
+
+    while (num >= 1) {
+        console.log(num);
+        num--;
+    }
+}
