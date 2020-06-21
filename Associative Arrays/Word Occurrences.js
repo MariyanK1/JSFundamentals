28eaeb56af1f573c21ecb9d08d1b8d1025cc49c4
MariyanMaksimov <58628678+MariyanK1@users.noreply.github.com>
2020-06-21 20:31:01 +0200

    Create Word Occurrences.js

---
+++ Associative Arrays/Word Occurrences.js
@@ -0,0 +1,45 @@
+/*
+Write a function that counts the times each word occurs in a text. 
+Print the words sorted by count in descending order. 
+The input comes as an array of strings.
+
+Sample case:
+
+In:
+["Here", "is", "the", "first", "sentence", "Here", "is", "another", "sentence",
+"And", "finally", "the", "third", "sentence"]
+
+Out:
+
+sentence -> 3 times
+Here -> 2 times
+is -> 2 times
+the -> 2 times
+first -> 1 times
+another -> 1 times
+And -> 1 times
+finally -> 1 times
+third -> 1 times
+
+*/
+
+function slv(input) {
+
+    let map = new Map();
+
+    for (const x of input) {
+
+        if (!map.has(x)) {
+            map.set(x, 1);
+        } else {
+            let value = map.get(x);
+            map.set(x, value += 1);
+        }
+    }
+
+    let result = Array.from(map).sort((a, b) => b[1] - a[1]);
+
+    for (const x of result) {
+        console.log(`${x[0]} -> ${x[1]} times`);
+    }
+}
