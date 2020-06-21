8514cdb0279533ccecb23c4872973b7b8e06bd29
MariyanMaksimov <58628678+MariyanK1@users.noreply.github.com>
2020-06-21 16:20:29 +0200

    Create Biggest of 3 Numbers.js

---
+++ Data Types and Variables/Biggest of 3 Numbers.js
@@ -0,0 +1,14 @@
+function solve(n1, n2, n3) {
+
+    if (n1 > n2 && n1 > n3) {
+        console.log(n1);
+    }
+
+     if (n2 > n1 && n2 > n3) {
+        console.log(n2);
+    }
+
+     if (n3 > n1 && n3 > n2) {
+        console.log(n3);
+    }
+}
