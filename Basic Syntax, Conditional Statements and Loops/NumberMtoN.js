da0d1dc04a6cd4e7089b867221343ac52116a73b
MariyanMaksimov <58628678+MariyanK1@users.noreply.github.com>
2020-06-21 15:15:04 +0200

    Create NumberMtoN.js

---
+++ Basic Syntax, Conditional Statements and Loops/NumberMtoN.js
@@ -0,0 +1,10 @@
+/*
+Create a function that receives a number N and prints all the numbers from N to 1. Try using while loop
+*/
+
+function tralala (m, n) {
+
+    for (let index = m; index >= n; index--) {
+        console.log(index) 
+    }
+}
