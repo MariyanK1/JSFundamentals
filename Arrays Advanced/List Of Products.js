456c5a571ec06cdd6d5b9f307dcdeb67962fb9a8
MariyanMaksimov <58628678+MariyanK1@users.noreply.github.com>
2020-06-21 18:56:24 +0200

    Update List Of Products.js

--- Arrays Advanced/List Of Products.js
+++ Arrays Advanced/List Of Products.js
@@ -3,9 +3,7 @@
 
 function solve(array = []) {
     let sorted = array.sort()
-    let n = 0;
     for (let i = 0; i < sorted.length; i++) {
-        n++;
-        console.log(`${n}.${sorted[i]}`);
+        console.log(`${i+1}.${sorted[i]}`);
     }
 }
