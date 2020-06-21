71b8fdd24b5786ed9b62200c29aeaf2fe2adc5e8
MariyanMaksimov <58628678+MariyanK1@users.noreply.github.com>
2020-06-21 16:27:34 +0200

    Create Equal Arrays.js

---
+++ Arrays/Equal Arrays.js
@@ -0,0 +1,15 @@
+function solve(arr1, arr2) {
+    let sum = 0;
+    for (let i in arr1) {
+        let firstEl = arr1[i];
+        let secondEl = arr2[i];
+        if (firstEl !== secondEl) {
+            console.log(`Arrays are not identical. Found difference at ${i} index`);
+            return;
+        }
+
+        sum += +firstEl;
+    }
+
+    console.log(`Arrays are identical. Sum: ${sum}`);
+}
