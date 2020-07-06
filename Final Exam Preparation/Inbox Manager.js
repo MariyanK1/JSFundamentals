cf17d01cd6d2e9296c10812c90304f5bc14e0e02
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-06 21:02:17 +0200

    Update Inbox Manager.js

--- Final Exam Preparation/Inbox Manager.js
+++ Final Exam Preparation/Inbox Manager.js
@@ -1,3 +1,58 @@
+/*
+Inbox Manager
+Create a program that manages users and Emails sent by users. You need to keep information about their username and their sent Emails.
+The Emails are represented as strings. You will be receiving lines with commands separated by "->" until you receive the "Statistics" command.
+
+There are three possible commands:
+"Add->{username}":
+Check if the username exists and if it does print:
+"{username} is already registered"
+ If it doesn’t exist, then add the user to the collection of users.
+
+"Send->{username}->{Email}"
+Add the {Email} to the {username}'s collection of sent Emails.
+"Delete->{username}":
+
+Delete the given user, if he exists. If the user doesn’t exist, print:
+"{username} not found!"
+
+In the end, you have to print the count of users, each user with his/her Emails. Users need to be sorted in descending order by 
+the count of sent Emails and then by their username in ascending order in the following format:  
+Users count: {count}
+{username} 
+ - {Email1}
+ - {Email2}
+ - {Emailn}
+
+Input
+You will be receiving lines until you receive the "Statistics" command.
+The commands will be in the format described above.
+
+Output
+Print the collection in the format described above after the "Statistics" command. 
+
+Sample case:
+
+In:
+Add->Mike
+Add->George
+Send->George->Hello World
+Send->George->Some random test mail
+Send->Mike->Hello, do you want to meet up tomorrow?
+Send->George->It would be a pleasure
+Send->Mike->Another random test mail
+Statistics
+
+Out:
+Users count: 2
+George
+ - Hello World
+ - Some random test mail
+ - It would be a pleasure
+Mike
+ - Hello, do you want to meet up tomorrow?
+ - Another random test mail
+*/
 function inboxManager(input = []) {
     let betterInput = input.slice(0, input.indexOf('Statistics'));
     let obj = {};
