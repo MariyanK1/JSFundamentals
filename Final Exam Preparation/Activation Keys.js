08c25d0f08e13b2be05bf1da3dee9522d5bc03d0
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-06 21:04:47 +0200

    Update Activation Keys.js

--- Final Exam Preparation/Activation Keys.js
+++ Final Exam Preparation/Activation Keys.js
@@ -1,3 +1,51 @@
+/*
+The first line of the input will be your raw activation key. It will consist of letters and numbers only. 
+After that, until the "Generate" command is given, you will be receiving strings with instructions 
+for different operations that need to be performed upon the raw activation key.
+
+There are several types of instructions, split by ">>>":
+
+Contains>>>{substring} – checks if the raw activation key contains the given substring.
+If it does prints: "{raw activation key} contains {substring}".
+If not, prints: "Substring not found!"
+
+Flip>>>Upper/Lower>>>{startIndex}>>>{endIndex}
+Changes the substring between the given indices (the end index is exclusive) to upper or lower case. 
+All given indexes will be valid.
+Prints the activation key.
+
+Slice>>>{startIndex}>>>{endIndex}
+Deletes the characters between the start and end indices (end index is exclusive).
+Both indices will be valid.
+Prints the activation key.
+
+Input
+The first line of the input will be string and it will consist of letters and numbers only. 
+After the first line, until the "Generate" command is given, you will be receiving strings.
+
+Output
+After the "Generate" command is received, print:
+"Your activation key is: {activation key}"
+
+Sample Case:
+
+In:
+abcdefghijklmnopqrstuvwxyz
+Slice>>>2>>>6
+Flip>>>Upper>>>3>>>14
+Flip>>>Lower>>>5>>>7
+Contains>>>def
+Contains>>>deF
+Generate
+
+Out:
+abghijklmnopqrstuvwxyz
+abgHIJKLMNOPQRstuvwxyz
+abgHIjkLMNOPQRstuvwxyz
+Substring not found!
+Substring not found!
+Your activation key is: abgHIjkLMNOPQRstuvwxyz
+*/
 function activationKeys(input = []) {
     let inputString = String(input.shift());
     let validInput = input.slice(0, input.indexOf('Generate'));
