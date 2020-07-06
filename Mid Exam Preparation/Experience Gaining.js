1ddfd243dcce171bb42b2c8435a8b74a055a4aed
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-06 21:31:06 +0200

    Update Experience Gaining.js

--- Mid Exam Preparation/Experience Gaining.js
+++ Mid Exam Preparation/Experience Gaining.js
@@ -1,3 +1,50 @@
+/*
+Write a program, that helps a player figure how many battles he will need to play in a battle video game,
+in order to unlock the next tank in the line.
+
+On the first line you will receive the amount of experience that is needed to unlock the tank.
+On the second line you will receive the count of battles.
+On the next lines, you will receive the experience the player can gain in every battle.
+
+Calculate if he can unlock the tank. Keep in mind that he gets 15% more experience for every third battle and 10% less for every fifth battle.
+You also need to stop the program as soon as he collects the needed experience.
+
+If he managed to gather the experience, print how many battles it took him in the following format:
+"Player successfully collected his needed experience for {battleCount} battles."
+
+If he was not able to do it, print the following message:
+"Player was not able to collect the needed experience, {neededExperience} more needed."
+Format the needed experience to the second decimal place.
+
+Input
+On the first line you will receive the needed experience amount –  a real number in the range [0.0….400000.0]
+On the second line you will receive the count of battles – an integer number in the range 
+[0….500]
+On the next lines you will receive the experience earned per battle – a real number in the range 
+[0.0….5000.0]
+
+Output
+If he managed to gather the experience:
+"Player successfully collected his needed experience for {battleCount} battles."
+If he was not able to do it:
+"Player was not able to collect the needed experience, {neededExperience} more needed."
+NOTE: Format the needed experience to the second decimal place.
+
+Sample Case:
+
+In:
+500
+5
+50
+100
+200
+100
+30
+
+Out:
+Player successfully collected his needed experience for 5 battles.
+*/
+
 function solve(arr) {
     arr = arr.map(Number);
     let neededPoints = arr.shift();
