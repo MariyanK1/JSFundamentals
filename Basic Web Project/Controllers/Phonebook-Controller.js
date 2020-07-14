6ff68198e1f46b54c98eff08054ef8313e464284
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-14 12:45:09 +0200

    Update Phonebook-Controller.js

--- Basic Web Project/Controllers/Phonebook-Controller.js
+++ Basic Web Project/Controllers/Phonebook-Controller.js
@@ -1,11 +1,22 @@
 const phonebook = require('../phonebook');
+const Contact = require('../models/Contact');
 
 module.exports = {
+  // TODO: load index page
   index: (req, res) => {
-    res.render('index');
-    // TODO: load index page
+    let contacts = phonebook.getPhonebook();
+    res.render('index', {contacts});
+    
   },
+
   addPhonebookPost:(req, res) => {
     // TODO: add a phonebook object to the array
+    let {name, number} = req.body;
+    
+    let contact = new Contact(name, number);
+
+    phonebook.addContact(contact);
+
+    res.redirect('/');
   }
 }
