186830327f3ac74bdc3f2f333cf8377e3f992486
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-14 12:24:39 +0200

    Update phonebook.js

--- Basic Web Project/phonebook.js
+++ Basic Web Project/phonebook.js
@@ -3,3 +3,18 @@
 	add methods for adding in the phonebook and getting it
 	export the methods
 */
+
+const phonebook = [];
+
+function getPhonebook() {
+	return phonebook.slice();
+}
+
+function addContact(contact) {
+	phonebook.push(contact)
+}
+
+module.exports = {
+	getPhonebook,
+	addContact
+}
