8ee5cb29ed60850ad58a29de110be642635b0c21
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-22 19:05:02 +0200

    Update Length Limit.js

--- Object and Classes/Length Limit.js
+++ Object and Classes/Length Limit.js
@@ -25,3 +25,35 @@ console.log(test.toString()); // ...
 test.increase(4); 
 console.log(test.toString()); // Test
 */
+
+class Stringer{
+    constructor(string, length){
+        this.innerString = string;
+        this.innerLength = length;
+    }
+
+    increase(length){
+        this.innerLength += length;
+        if(this.innerLength < 3){
+            this.innerLength = 0;
+        }
+    }
+
+    decrease(length){
+        this.innerLength -= length;
+        if(this.innerLength < 3){
+            this.innerLength = 0;
+        }
+    }
+
+    toString(){
+        if(this.innerLength == 0){
+            return "...";
+        }
+
+        if(this.innerString.length > this.innerLength){
+            this.innerString = this.innerString.substr(0, this.innerLength) + "...";
+        }
+        return this.innerString;
+    }
+}
