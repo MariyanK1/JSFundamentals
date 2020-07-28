dea444e1dbda2caba128e76cc19a79080594f9b2
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-07-28 20:26:10 +0200

    Create seek&destroy.js

---
+++ Basic Syntax, Conditional Statements and Loops/seek&destroy.js
@@ -0,0 +1,21 @@
+/*
+You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments.
+Remove all elements from the initial array that are of the same value as these arguments.
+*/
+
+function destroyer(...args) {
+
+    let arr = Array.from(args.shift());
+
+    while (args.length > 0) {
+        let el = args.shift();
+
+        while (arr.includes(el)) {
+            arr.splice(arr.indexOf(el), 1)
+        }
+    }
+
+    return arr
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
