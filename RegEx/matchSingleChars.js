fdbd4df37f94eefa921d2d15ca7385b8a4eb2e0e
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-07-30 10:15:13 +0200

    Create matchSingleChars.js

---
+++ RegEx/matchSingleChars.js
@@ -0,0 +1,10 @@
+/*
+Create a single regex that matches all characters that are not a number or a vowel.
+Remember to include the appropriate flags in the regex.
+*/
+
+let quote = "3 blind mice.";
+let myRegex = /([^\d])/gi;
+let result = quote.match(myRegex);
+
+console.log(result);
