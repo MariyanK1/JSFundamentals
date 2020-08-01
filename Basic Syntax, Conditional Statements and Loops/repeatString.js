e3a690172ca4bc225ff5b22ae783ed2cc148d8c8
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-01 08:59:19 +0200

    Create repeatString.js

---
+++ Basic Syntax, Conditional Statements and Loops/repeatString.js
@@ -0,0 +1,15 @@
+/*
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+*/
+
+function repeatStringNumTimes(str, num) {
+    let newStr = ''
+
+    for (let i = 1; i <= num; i++) {
+        newStr += str;        
+    }
+
+    return newStr
+}
+
+repeatStringNumTimes("abc", 3);
