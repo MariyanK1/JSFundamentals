f27217498c524290ff43d54e20738bfc5bba7bad
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-02 20:33:39 +0200

    Update Prime Number Checker.js

--- Data Types and Variables/Prime Number Checker.js
+++ Data Types and Variables/Prime Number Checker.js
@@ -1,22 +1,19 @@
-function test_prime(n)
-{
-
-  if (n===1)
-  {
+function test_prime(n) {
+  
+  if (n===1){
     return false;
-  }
-  else if(n === 2)
-  {
+  } 
+  
+  else if(n === 2){
     return true;
-  }else
-  {
-    for(var x = 2; x < n; x++)
-    {
-      if(n % x === 0)
-      {
+  }
+  
+  else {
+  for(let x = 2; x < n; x++){      
+      if (n % x === 0){
         return false;
       }
-    }
+  }
     return true;  
   }
 }
