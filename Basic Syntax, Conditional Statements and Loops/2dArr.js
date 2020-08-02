74212fb9be620562697ee1c3631f69ab95af81ca
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-02 19:17:15 +0200

    Create 2dArr.js

---
+++ Basic Syntax, Conditional Statements and Loops/2dArr.js
@@ -0,0 +1,14 @@
+/*
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+*/
+
+
+function chunkArrayInGroups(arr, size) {
+    var newArr = [];
+    while (arr.length) {
+        newArr.push(arr.splice(0, size));
+    }
+    return newArr;
+}
+
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)
