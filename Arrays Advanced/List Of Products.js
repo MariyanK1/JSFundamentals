f5be4a045f24701c2c18730e615af290e1c8c50e
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-02 20:36:39 +0200

    Update List Of Products.js

--- Arrays Advanced/List Of Products.js
+++ Arrays Advanced/List Of Products.js
@@ -2,7 +2,9 @@
 //Print a numbered array of all the products ordered by name.
 
 function solve(array = []) {
+    
     let sorted = array.sort()
+    
     for (let i = 0; i < sorted.length; i++) {
         console.log(`${i+1}.${sorted[i]}`);
     }
