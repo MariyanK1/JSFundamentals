ed8310d265611a4825f410139da5651e03855aa7
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-04 11:41:44 +0200

    Update removeWhitespace.js

--- RegEx/removeWhitespace.js
+++ RegEx/removeWhitespace.js
@@ -1,1 +1,9 @@
-//TO DO
+/*
+Write a regex and use the appropriate string methods to remove whitespace at the beginning and end of strings.
+
+Note: The String.prototype.trim() method would work here, but you'll need to complete this challenge using regular expressions.
+*/
+
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+(\w+,\s\w+!)\s+$/.exec(hello); // Change this line
+let result = wsRegex[1];// Change this line
