1a07df2d7d8bba66a211ba406a81854438375915
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-04 09:49:30 +0200

    Update PositiveNegativeLookahead.js

--- RegEx/PositiveNegativeLookahead.js
+++ RegEx/PositiveNegativeLookahead.js
@@ -1,1 +1,8 @@
-//TO DO
+/*
+Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long,
+do not begin with numbers, and have two consecutive digits.
+*/
+
+let sampleWord = "bana12";
+let pwRegex = /^\D(?=\w{5})(?=\w*\d{2})/;
+let result = pwRegex.test(sampleWord);
