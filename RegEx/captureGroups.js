56a56a577752cca979097f71073c2d80d182ad02
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-04 10:38:20 +0200

    Update captureGroups.js

--- RegEx/captureGroups.js
+++ RegEx/captureGroups.js
@@ -1,1 +1,7 @@
-//TO DO
+/*
+Use capture groups in reRegex to match numbers that are repeated only three times in a string, each separated by a space.
+*/
+
+let repeatNum = "42 42 42";
+let reRegex = /(^\d+)\s\1\s\1$/;
+let result = reRegex.test(repeatNum)
