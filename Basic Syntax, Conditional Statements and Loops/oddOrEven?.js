67b15475ff47527d695625e2544ae2ff01c4eb70
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-06 21:23:46 +0200

    Create oddOrEven?.js

---
+++ Basic Syntax, Conditional Statements and Loops/oddOrEven?.js
@@ -0,0 +1,29 @@
+/*
+Task:
+Given a list of numbers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+*/
+
+
+function oddOrEven(array) {
+    let sum = 0;
+
+    if (array.length <= 0) {
+        return 'even'
+    }
+    
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];        
+    }
+
+    if (sum % 2 === 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
+} 
+
+odd_or_even([0, -1, -5])
