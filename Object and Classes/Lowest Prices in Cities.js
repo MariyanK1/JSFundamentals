a92e84be5b9e20428981e34eae2ac0d1c8babc64
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-09 20:16:04 +0200

    Update Lowest Prices in Cities.js

--- Object and Classes/Lowest Prices in Cities.js
+++ Object and Classes/Lowest Prices in Cities.js
@@ -2,13 +2,13 @@
 You will be given several towns, with products and their price. You need to find the lowest price
 for every product and the town it is sold at for that price.
 
-Input
+Input:
 The input comes as array of strings. Each element will hold data about a town, product, and its price at that town.
 The town and product will be strings, the price will be a number. The input will come in the following format:
 {townName} | {productName} | {productPrice}
 If you receive the same town and product more than once, you should update the old value with the new one.
 
-Output
+Output:
 As output you must print each product with its lowest price and the town at which the product is sold at that price.
 If two towns share the same lowest price, print the one that was entered first. 
 The output, for every product, should be in the following format:
