2214427e08abcdef9e479bdb32f637e8f3904781
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-11 19:36:30 +0200

    Update Count String Occurrences.js

--- Text Processing/Count String Occurrences.js
+++ Text Processing/Count String Occurrences.js
@@ -1,7 +1,7 @@
 //Write a function that receives a text and a string that you need to search.
 //Print all the occurrences of that word in the string.
 
-function solve(text = String, searchedWord) {
+function solve(text, searchedWord) {
     let words = text.split(' ');
     let counter = 0;
 
