1fc458d237a84015e69c5bc36e516defcabbd0a4
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-08-11 19:36:03 +0200

    Update Censored Words.js

--- Text Processing/Censored Words.js
+++ Text Processing/Censored Words.js
@@ -1,7 +1,7 @@
 //Write a function that receives a text as a first parameter  and a single word as a second. 
 //Find all occurrences of that word in the text and replace them with the corresponding count of '*'.
 
-function solve(text = String, word) {
+function solve(text, word) {
     while (text.includes(word)) {
         text = text.replace(word, '*'.repeat(word.length));
     }
