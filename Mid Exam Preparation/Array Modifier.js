71b99d796a62c654920601e876f6752a713a722b
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-10-17 20:05:27 +0200

    Update Array Modifier.js

--- Mid Exam Preparation/Array Modifier.js
+++ Mid Exam Preparation/Array Modifier.js
@@ -41,34 +41,34 @@ Out:
 
 */
 function x(input = []) {
-    let inputArr = input.shift().split(' ').map(Number);
-    let betterInput = input.slice(0, input.indexOf('end'));
+   let inputArr = input.shift().split(' ').map(Number);
+   let betterInput = input.slice(0, input.indexOf('end'));
 
-    for (const line of betterInput) {
-        let [command, index, index2] = line.split(' ');
-        index = Number(index)
-        index2 = Number(index2)
+   for (const line of betterInput) {
+      let [command, index, index2] = line.split(' ');
+      index = Number(index);
+      index2 = Number(index2);
 
-        switch (command) {
-            case 'swap':
-                let el1 = inputArr[index];
-                let el2 = inputArr[index2];
+      switch (command) {
+         case 'swap':
+            let el1 = inputArr[index];
+            let el2 = inputArr[index2];
 
-                inputArr[index] = el2;
-                inputArr[index2] = el1;
-                break;
+            inputArr[index] = el2;
+            inputArr[index2] = el1;
+            break;
 
-            case 'multiply':
-                inputArr[index] *= inputArr[index2]
-                break;
+         case 'multiply':
+            inputArr[index] *= inputArr[index2]
+            break;
 
-            case 'decrease':
-                for (let i = 0; i < inputArr.length; i++) {
-                    inputArr[i] -= 1;
-                }
-                break;
+         case 'decrease':
+            for (let i = 0; i < inputArr.length; i++) {
+               inputArr[i] -= 1;
+            }
+            break;
 
-        }
-    }
-    console.log(inputArr.join(', '));
+      }
+   }
+   return inputArr.join(', ');
 }
