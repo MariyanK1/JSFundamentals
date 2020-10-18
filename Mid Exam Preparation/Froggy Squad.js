b4d2b05d54df21955bbccf13f265301224803d66
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-10-18 22:12:03 +0200

    Update Froggy Squad.js

--- Mid Exam Preparation/Froggy Squad.js
+++ Mid Exam Preparation/Froggy Squad.js
@@ -50,38 +50,49 @@ Frogs: Kvachko Kishko Muggy
 function froggySquad(arr) {
     let output = '';
     let names = arr.shift().split(' ');
+    
     for (let iterator of arr) {
         let command = iterator.split(' ');
         let firstWord = command.shift();
         let secondWord = command.shift();
- 
-        if (firstWord === 'Join') {
-            names.push(secondWord);
-        } else if (firstWord === 'Jump') {
-            let num = command.shift();
-            if (num >= 0 && num <= names.length) {
-                names.splice(Number(num), 0, secondWord);
-            }
-        } else if (firstWord === 'Dive') {
-            let num = Number(secondWord);
-            if (num >= 0 && num < names.length) {
-                names.splice(num, 1);
-            }
-        } else if (firstWord === 'First') {
-            let count = Number(secondWord);
-            console.log(names.slice(0, count).join(' '));
- 
-        } else if (firstWord === 'Last') {
-            let count = Number(secondWord);
-            console.log(names.slice(-count).join(' '));
- 
-        } else if (firstWord === 'Print') {
-            if (secondWord === 'Reversed') {
-                output = `Frogs: ${names.reverse().join(' ')}`;
-            } else if (secondWord === 'Normal') {
-                output = `Frogs: ${names.join(' ')}`;
-            }
-            console.log(output);
+
+        switch (firstWord) {
+            case 'Join':
+                names.push(secondWord);
+                break;
+            
+            case 'Jump':
+                let num = command.shift();
+                if (num >= 0 && num <= names.length) {
+                    names.splice(Number(num), 0, secondWord);
+                }
+                break;
+            
+            case 'Dive':
+                let num = Number(secondWord);
+                if (num >= 0 && num < names.length) {
+                    names.splice(num, 1);
+                }
+                break;
+            
+            case 'First':
+                let count = Number(secondWord);
+                console.log(names.slice(0, count).join(' '));
+                break;
+            
+            case 'Last':
+                let count = Number(secondWord);
+                console.log(names.slice(-count).join(' '));
+                break;
+            
+            case 'Print':
+                if (secondWord === 'Reversed') {
+                    output = `Frogs: ${names.reverse().join(' ')}`;
+                } else if (secondWord === 'Normal') {
+                    output = `Frogs: ${names.join(' ')}`;
+                }
+                console.log(output);
+                break;
         }
     }
 }
