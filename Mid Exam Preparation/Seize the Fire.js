5e59f66d95df441cac73a3c04eff73e8a5290c83
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-10-19 20:04:02 +0200

    Update Seize the Fire.js

--- Mid Exam Preparation/Seize the Fire.js
+++ Mid Exam Preparation/Seize the Fire.js
@@ -73,45 +73,62 @@ Total Fire: 217
 
 
 function solve(input = []) {
-  let fire = input.shift().split("#");
-  let water = +input.shift();
-  let effort = 0;
-  let sumFire = 0;
-  console.log(`Cells:`);
-  
-  
-  while (fire.length > 0) {
-    let cells = fire.shift().split(" = ");
-    let command = cells[0];
-    let value = +cells[1];
-  
-  if (command === "High" && value >= 81 && value <= 125) {
-      water -= value;
-      if (water < 0) {
-        break;
-      }
-      effort += value * 0.25;
-      sumFire += value;
-      console.log(` - ${value}`);
-    } else if (command === "Medium" && value >= 51 && value <= 80) {
-      water -= value;
-      if (water < 0) {
-        break;
-      }
-      effort += value * 0.25;
-      sumFire += value;
-      console.log(` - ${value}`);
-    } else if (command === "Low" && value >= 1 && value <= 50) {
-      water -= value;
-      if (water < 0) {
-        break;
-      }
-      effort += value * 0.25;
-      sumFire += value;
-      console.log(` - ${value}`);
-    }
-  }
+    let fire = input.shift().split("#");
+    let water = Number(input.shift());
+    let effort = 0;
+    let sumFire = 0;
+    console.log(`Cells:`);
 
-  console.log(`Effort: ${effort.toFixed(2)}`);
-  console.log(`Total Fire: ${sumFire}`);
+
+    while (fire.length > 0) {
+        let cells = fire.shift().split(" = ");
+        let command = cells[0];
+        let value = Number(cells[1]);
+
+        switch (command) {
+            case 'High':
+                if (value >= 81 && value <= 125) {
+                    
+                    water -= value;
+                    
+                    if (water < 0) {
+                        break;
+                    }
+                    
+                    effort += value * 0.25;
+                    sumFire += value;
+                    console.log(` - ${value}`);
+                }
+                break;
+            
+            case 'Medium':
+                if (value >= 51 && value <= 80) {
+                    water -= value;
+                    
+                    if (water < 0) {
+                        break;
+                    }
+
+                    effort += value * 0.25;
+                    sumFire += value;
+                    console.log(` - ${value}`);
+                }
+                break;
+
+            case 'Low':
+                if (value >= 1 && value <= 50) {
+                    water -= value;
+                    if (water < 0) {
+                        break;
+                    }
+                    effort += value * 0.25;
+                    sumFire += value;
+                    console.log(` - ${value}`);
+                }
+                break;
+        }
+    }
+
+    console.log(`Effort: ${effort.toFixed(2)}`);
+    console.log(`Total Fire: ${sumFire}`);
 }
